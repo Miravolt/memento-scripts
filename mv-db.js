a998da7591b0be4da05071b852c233e8c23ed0c1
Jimmy Josefsson <jimmy.josefsson@miravolt.se>
2026-09-21 10:31:44 +0200

    Flytta förutsättningar före stegen och skriv ut modullistan på plats

--- mv-db.js
+++ mv-db.js
@@ -297,4 +297,4 @@ MV.db.copyFields = function (sourceEntry, fieldNames) {
 
 // byggstämpel — skrivs av tools/stamp.js
 MV.build = MV.build || { moduler: [] };
-MV.build.moduler.push({ namn: "mv-db", byggd: "2026-09-15 15:59", hash: "4955439" });
+MV.build.moduler.push({ namn: "mv-db", byggd: "2026-09-21 10:31", hash: "4955439" });
